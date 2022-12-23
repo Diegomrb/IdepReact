a8e73190751bd2c71ab093289f8f67cf3e898559
gmorales254 <55248854+gmorales254@users.noreply.github.com>
2022-12-22 22:48:09 -0300

    agrego alerta y botonera "ida y vuelta"

--- frontend/src/components/MarcarPasaje/MarcarPasaje.jsx
+++ frontend/src/components/MarcarPasaje/MarcarPasaje.jsx
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { Select, Stack } from "@chakra-ui/react";
+import { Select, Stack, Box, useDisclosure } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton
+} from '@chakra-ui/react'
 
 const url = "http://localhost:3000";
 
 export function MarcarPasaje() {
+
   const [localidades, setLocalidades] = useState([]);
   const [origen, setOrigen] = useState("");
   const [destino, setDestino] = useState("");
+  const [idaVuelta, setIdaVuelta] = useState(true);
+  const [mostrarAlerta, setMostrarAlerta] = useState(false);
+
   useEffect(() => {
     fetch(`${url}/localidades`, {
       method: "GET",
@@ -31,13 +42,20 @@ export function MarcarPasaje() {
     })
       .then((respuesta) => respuesta.json())
       .then((datos) => {
-        console.log(datos);
-        /* setLocalidades(datos); */
+        !datos.length ? setMostrarAlerta(true) : setMostrarAlerta(false);
       });
   }
 
   return (
-    <Stack direction={["column", "row"]}>
+    <Stack direction={["column"]} gap={3}>
+      <Stack direction='row' spacing={4}>
+        <Button colorScheme='teal' onClick={() => setIdaVuelta(false)} variant={!idaVuelta ? 'solid' : 'outline'}>
+          Solo ida
+        </Button>
+        <Button colorScheme='teal' onClick={() => setIdaVuelta(true)} variant={idaVuelta ? 'solid' : 'outline'}>
+          Ida y vuelta
+        </Button>
+      </Stack>
       <Select onChange={(e) => setOrigen(e.target.value)} placeholder="Origen">
         {localidades.map((item) => (
           <option key={item["_id"]} value={item["_id"]}>
@@ -59,6 +77,35 @@ export function MarcarPasaje() {
       <Button onClick={irAViajes} colorScheme="blackAlpha">
         Buscar Pasaje
       </Button>
+
+      {mostrarAlerta &&
+        <Alerta mostrar={setMostrarAlerta} />
+      }
     </Stack>
   );
+}
+
+
+function Alerta({ mostrar }) {
+
+
+
+  return (
+    <Alert status='info'>
+      <AlertIcon />
+      <Box>
+        <AlertTitle>Ups!</AlertTitle>
+        <AlertDescription>
+          Parece que no tenemos pasajes disponibles para este origen y destino...
+        </AlertDescription>
+      </Box>
+      <CloseButton
+        alignSelf='flex-start'
+        position='relative'
+        right={-1}
+        top={-1}
+        onClick={() => mostrar(false)}
+      />
+    </Alert>
+  )
 }
