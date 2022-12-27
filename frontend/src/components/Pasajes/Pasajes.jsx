96764133d266bddc2817a4af9a4f1da57098763b
Diegomrb <75950612+Diegomrb@users.noreply.github.com>
2022-12-27 18:06:56 -0300

    Update Pasajes.jsx

--- frontend/src/components/Pasajes/Pasajes.jsx
+++ frontend/src/components/Pasajes/Pasajes.jsx
@@ -9,6 +9,8 @@ import {
   CardBody,
   CardHeader,
   Stack,
+  Text,
+  SimpleGrid,
 } from "@chakra-ui/react";
 
 export function Pasajes(info) {
@@ -20,10 +22,31 @@ export function Pasajes(info) {
         margin={"8px"}
         /* onClick={() => navigate(`/viaje/${info._id}/${tipoViaje}`)} */
       >
-        <CardHeader textAlign={"center"}>
+        <CardHeader>
           {/* {<img style={{ width: "50%" }} src={logo} alt={nombreEmp} />} */}
+          <img
+            style={{ width: "50%" }}
+            src="/frontend/public/logoEmpresas/cot.png"
+            alt="logoempresa"
+          />
         </CardHeader>
-        <CardBody className="card-body-container"></CardBody>
+        <CardBody className="card-body-container">
+          <SimpleGrid columns={2} spacing={10}>
+            <Text fontSize="3x1" as="b">
+              Origen
+            </Text>
+            <Text fontSize="3x1" as="b">
+              Destino
+            </Text>
+            <Text>Viernes 18 de septiembre</Text>
+            <Text>Viernes 18 de septiembre</Text>
+            <Text>Pasajero</Text>
+            <Text>Monto total</Text>
+            <Text>Numero de ticket</Text>
+            <Text>Bus</Text>
+            <Text>Asientos</Text>
+          </SimpleGrid>
+        </CardBody>
       </Card>
     </Stack>
   );
